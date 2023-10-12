660594a9413324e05185fa9dbfe0a002586fd5c6
saiMahadasa <saimahadasa1999@gmail.com>
2023-10-11 23:07:29 -0500

    bonus added

--- js/penalty_script.js
+++ js/penalty_script.js
@@ -1,11 +1,13 @@
-let ids = ["id1", "id2", "id3","id4", "id5", "id6","id7", "id8", "id9","id10", "id11", "id12","id13", "id14", "id15", "id16", "id17", "id18","id19", "id20", "id21","id22", "id23", "id24", "id25" ];
-let checkCount = 0;
-let penalty = 0;
-let lastCheck = 0;
-let treasureLocation = 5;
-let trapLocation = 18;
-let gameOver = false;
 
+var ids = ["id1", "id2", "id3","id4", "id5", "id6","id7", "id8", "id9","id10", "id11", "id12","id13", "id14", 
+"id15", "id16", "id17", "id18","id19", "id20", "id21","id22", "id23", "id24", "id25" ];
+var checkCount = 0;
+var penalty = 0;
+var lastCheck = 0;
+var treasureLocation = Math.floor(Math.random()*(25-0) - 0);
+var trapLocation = Math.floor(Math.random()*(25-0)-0);
+var trapLocation = 18;
+var gameOver = false;
 
 function check(position){
     if(gameOver){
@@ -14,10 +16,10 @@ function check(position){
 
     var myimage= ids[position];
 
-    var imageElement = document.getElementById(myimage);
+    var imageElmt = document.getElementById(myimage);
 
     if(position === treasureLocation){
-      imageElement.src = "./images/image3.webp";
+      imageElmt.src = "./images/image3.webp";
       checkCount++;
       penalty++;
       document.getElementById("locations").textContent="Location Checked: " + checkCount;
@@ -25,7 +27,7 @@ function check(position){
       penalty -=3;
     }
     else if(position === trapLocation) {
-      imageElement.src="./images/image4.webp";
+      imageElmt.src="./images/image4.webp";
       checkCount++;
       penalty++;
       document.getElementById("locations").textContent="Location Clicked : " + checkCount;
@@ -33,7 +35,7 @@ function check(position){
       penalty +=4;
     }
     else{
-      imageElement.src="./images/image5.jpg";
+      imageElmt.src="./images/image5.jpg";
       checkCount++;
       penalty++;
       document.getElementById("locations").textContent="Location Checked: " + checkCount;
@@ -45,20 +47,22 @@ function check(position){
 
   function help(){
 
+    
+
    var treasureRow=Math.floor(treasureLocation/5);
    var treasureColumn=Math.floor(treasureLocation%5)
    var lastRow = Math.floor(lastCheck / 5);
-   var lastCol = Math.floor(lastCheck % 5);
+   var lastColmn = Math.floor(lastCheck % 5);
   
-   var rowDiff = Math.abs(treasureRow-lastRow);
-   var colDiff = Math.abs(treasureColumn-lastCol);
+   var rowDifference = Math.abs(treasureRow-lastRow);
+   var colDifference = Math.abs(treasureColumn-lastColmn);
    var helpMessage = "";
 
-  if ((rowDiff <= 1 && colDiff <= 1) && !(rowDiff === 1 && colDiff === 1)) {
+  if ((rowDifference <= 1 && colDifference <= 1) && !(rowDifference === 1 && colDifference === 1)) {
       helpMessage = "Can smell it";
       penalty += 2;
 
-  } else if (rowDiff + colDiff === 2) {
+  } else if (rowDifference + colDifference === 2) {
       helpMessage = "Close Matie";
       penalty += 2;
 
@@ -70,6 +74,12 @@ function check(position){
   document.getElementById("help").textContent = helpMessage;
   }
 
-  function refresh(){
-      window.location.reload("Refresh")
-    }
+ 
+
+    //bonus
+
+
+
+function refresh() {
+  window.location.reload("Refresh")
+}
